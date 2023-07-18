ed0aa5fba53e722d4f0610a639452a8826a6c8ef
gitalexhubuser <alexdubovyck@yandex.ru>
2023-07-19 00:14:23 +0300

    яндекс диск русские символы(

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
 import tkinter as tk
+from Libs.lib_tk import TrainingPlanApp
 
-def create_training_table():
-    
-    # Получение введенного числа из поля ввода
-    weight = int(entry.get())
-    training_table = ""
-    
-    for week in range(1, 11):
-        training_weight = weight + (week - 1) * 10
-        training_table += f"{week} неделя {training_weight} кг 4 подхода по 8 раз\n"
-    
-    result_label.config(text=training_table)
-
-root = tk.Tk()
-root.title("Percentage in Powerlifting (PiP) ver 0.1")
-
-entry = tk.Entry(root)
-entry.pack()
-
-button = tk.Button(root, text="Посчитать", command=create_training_table)
-button.pack()
-
-result_label = tk.Label(root, text="")
-result_label.pack()
-
-root.mainloop()
-
+window = tk.Tk()
+app = TrainingPlanApp(window)
+window.mainloop()
